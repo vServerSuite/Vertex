542da056c3138c0034b14fb6f4fbfbd75bef8676
Ben Hall <benmhall1998@gmail.com>
2020-06-14 11:56:01 +0100

    Fixed entrypoint... again

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM mhart/alpine-node:12.18.0 AS build-env
 ADD bot /vertex
+
 WORKDIR /vertex
+
+RUN cp config/config.example.json ../config.example.json
 
 RUN yarn install --frozen-lockfile
 
--- bot/entrypoint.sh
+++ bot/entrypoint.sh
@@ -2,7 +2,7 @@
 if [ -f "/vertex/config/config.json" ]; then
   node src/index.js
 else
-  cp /vertex/config/config.example.json /vertex/config/config.json
-  rm /vertex/config/config.example.com
+  cp /vertex/config.example.json /vertex/config/config.json
+  rm /vertex/config.example.com
   node src/index.js
 fi
