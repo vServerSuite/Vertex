ce89a08174c58454b7f265e7bfab534701d45958
Ben Hall <me@benhall.co>
2020-06-14 11:39:54 +0100

    Create entrypoint.sh

---
+++ bot/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -f "/vertex/config/config.json" ]; then
+  node src/index.js
+else
+  cp /vertex/config/config.example.json /vertex/config/config.json
+  rm /vertex/config/config.example.com
+  node src/index.js
+fi
